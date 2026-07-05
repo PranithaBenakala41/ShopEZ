a79ad3db760eae97501b00c923367bc8feab98ac
Pranitha Benakala <benakalapranitha@gmail.com>
2026-07-05 09:30:57 +0530

    Clean up userRoutes.js by removing comments
    Removed commented sections for cleaner code.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,10 +5,8 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
-// =========================
 // REGISTER
-// =========================
+
 router.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -38,9 +36,8 @@ router.post("/register", async (req, res) => {
 });
 
 
-// =========================
 // LOGIN
-// =========================
+
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
